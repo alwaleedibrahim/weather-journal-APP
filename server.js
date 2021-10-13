68fbabe52faa380e6e6ef3b784f649dc7bc16ebd
alwaleedibrahim <alwaleed.ibrahim.m@gmail.com>
2021-10-13 23:22:48 +0200

    Added require body-parser and cors

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
+// Setup port
+const port = 8000;
+
 // Require Express to run server and routes
-const app = require("express");
+const express = require("express");
+
+// Requiure body-parser
 const bodyParser = require("body-parser");
+
+// require cors
+const cors = require("cors");
+
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
