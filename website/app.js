d85c5ec6df8270580236dce79f0e730fe66dc9d0
alwaleedibrahim <alwaleed.ibrahim.m@gmail.com>
2021-10-14 13:56:25 +0200

    Fixed incorrect month Added try, catch blocks to catch errors Added alert message when errors are found Added temprature units

--- website/app.js
+++ website/app.js
@@ -6,15 +6,21 @@ const apiKey = 'f25bd4111fdec5a8948fc32befe9c7d8';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + '/'+ d.getDate()+'/'+ d.getFullYear();
 
 // Load project data by fetching /getData route
 const loadProjectData = async () => {
     const getData = await (await fetch("/getData")).json();
-    // Loop over the data and dynamically add them to the page DOM structure
-    for (let prop in getData) {
-        const element = document.querySelector(`#${prop}`);
-        element.innerHTML = `<b>${prop}: </b>${getData[prop]}`;
+    try {
+        // Loop over the data and dynamically add them to the page DOM structure
+        for (let prop in getData) {
+            const element = document.querySelector(`#${prop}`);
+            element.innerHTML = `<b>${prop}: </b>${getData[prop]}`;
+        }
+        document.querySelector('.alert').innerHTML = "";
+    }
+    catch (err) {
+        document.querySelector('.alert').innerHTML = "Can not load data!"
     }
 }
 
@@ -29,22 +35,36 @@ const addData = async (data) => {
     });
 }
 
+// Use this to display temprature unit
+const tempUnits = {
+    "imperial": "Fahrenheit",
+    "standard": "Kelvin",
+    "metric": "Celsius"
+}
+
 // Add event listener to generate button
 document.querySelector('#generate').addEventListener('click', async () => {
     const zipCode = document.querySelector("#zip").value;
     const feelings = document.querySelector('#feelings').value;
     const units = document.querySelector('#units').value;
     const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=${units}`;
-    const response = await (await fetch(url)).json();
-    const temp = response.main.temp;
-    const data = {
-        date: newDate,
-        temp: temp,
-        content: feelings,
-        name: response.name
+    try {
+        const response = await (await fetch(url)).json();
+        const temp = response.main.temp;
+        const data = {
+            date: newDate,
+            temp: temp,
+            content: feelings,
+            name: response.name,
+            unit: tempUnits[units]
+        }
+        addData(data);
+        loadProjectData();
     }
-    addData(data);
-    loadProjectData();
+    catch (err) {
+        document.querySelector('.alert').innerHTML = "Invalid entry!"
+    }
+    
 });
 
 // When page loads, display previously stored data
